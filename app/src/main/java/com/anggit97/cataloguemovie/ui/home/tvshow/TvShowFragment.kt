0d35887b34ac530ef4b60795b958b264eac4fdba
Anggit Prayogo <anggitprayogo@gmail.com>
2019-08-27 10:18:55 +0700

    initial commit

---
+++ app/src/main/java/com/anggit97/cataloguemovie/ui/home/tvshow/TvShowFragment.kt
@@ -0,0 +1,29 @@
+package com.anggit97.cataloguemovie.ui.home.tvshow
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.anggit97.cataloguemovie.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class TvShowFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tv_show, container, false)
+    }
+
+    companion object{
+
+        fun newInstance() = TvShowFragment()
+    }
+}
